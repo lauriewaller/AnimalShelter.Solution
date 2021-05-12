3d3b14f69b47f16ee1bb6a565c200a2db700e258
Laurie Waller <lauriewaller7@gmail.com>
2021-05-11 20:47:29 -0700

    add Animals Controller code
    Co-authored-by: Faisal Rana <inquisitive@gmail.com>

---
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using AnimalShelter.Models;
+
+
+namespace AnimalShelter.Controllers
+{
+  public class AnimalsController : Controller
+  {
+    private readonly AnimalShelterContext _db;
+
+    public AnimalsController(AnimalShelterContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Animal> model = _db.Animals.ToList();
+      return View(model);
+    }
+  }
+}
