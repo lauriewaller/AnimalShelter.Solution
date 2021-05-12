31e56f414641df98728cb90a193f48a56132d771
Laurie Waller <lauriewaller7@gmail.com>
2021-05-11 20:07:44 -0700

    add Animal class
    Co-authored-by: Faisal Rana <inquisitive@gmail.com>

---
+++ AnimalShelter/Models/Animal.cs
@@ -0,0 +1,11 @@
+namespace AnimalShelter.Models
+{
+  public class Animal
+  {
+    public string Name { get; set; }
+    public string Breed { get; set; }
+    public string DateOfAdmittance { get; set; }
+    public string Gender { get; set; }
+  }
+
+}
