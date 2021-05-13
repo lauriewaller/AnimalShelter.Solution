45d8223edec645e4e5fece8fb41c31dc61ff4db9
Laurie Waller <lauriewaller7@gmail.com>
2021-05-12 20:02:20 -0700

    create Animals Controller Create route methods
    Co-authored-by: Faisal Rana <inquisitive@gmail.com>

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -20,5 +20,17 @@ namespace AnimalShelter.Controllers
       List<Animal> model = _db.Animals.ToList();
       return View(model);
     }
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Animal animal)
+    {
+      _db.Animals.Add(animal);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
