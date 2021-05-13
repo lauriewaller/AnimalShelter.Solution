6a65ca124c250d23bdef09fd7ac185ca59571264
Laurie Waller <lauriewaller7@gmail.com>
2021-05-12 21:00:42 -0700

    add AnimalId property to Animal class
    Co-authored-by: Faisal Rana <inquisitive@gmail.com>

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -2,6 +2,7 @@ namespace AnimalShelter.Models
 {
   public class Animal
   {
+    public int AnimalId { get; set; }
     public string Name { get; set; }
     public string Breed { get; set; }
     public string DateOfAdmittance { get; set; }
